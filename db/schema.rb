56e6b91f81a6479718ba511a998a5c5440e0eb27
chelahcynthia <cchelangat989@gmail.com>
2023-08-23 11:01:30 +0300

    Creates a comments controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_073455) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_22_070638) do
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "message_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["message_id"], name: "index_comments_on_message_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "messages", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -31,4 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_073455) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "messages"
+  add_foreign_key "comments", "users"
 end
